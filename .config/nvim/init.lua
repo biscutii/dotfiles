47720a142cc99428e1b59663c10e3baa51a032e3
biscutii <skinmaking@gmail.com>
2025-05-04 19:34:16 +0100

    Added some nvim plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,8 +2,21 @@
 require("config.lazy")
 
 require("catppuccin").setup({
-    transparent_background = true 
+    transparent_background = true, 
 })
 
 -- setup must be called before loading
 vim.cmd.colorscheme "catppuccin"
+
+require("nvim-treesitter.configs").setup({
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    }
+})
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.cmd("set number")
